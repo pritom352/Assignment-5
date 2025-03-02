6ad68004f7e8f55d718f6dffbff1eeca7508b4ae
pritom352 <pritomproshad@gmail.com>
2025-03-02 19:40:20 +0600

    done

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-// ----------first button---------------
+// ----------no: 1 button---------------
 
 document.getElementById('btn-1').addEventListener('click', function(event){
 
@@ -7,7 +7,7 @@ document.getElementById('btn-1').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -17,11 +17,9 @@ document.getElementById('btn-1').addEventListener('click', function(event){
     }
 
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-1");
@@ -29,16 +27,14 @@ document.getElementById('btn-1').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Fix Mobile Button Issue at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
-// ----------secend button---------------
+// ----------no: 2 button---------------
 
 document.getElementById('btn-2').addEventListener('click', function(event){
 
@@ -47,7 +43,7 @@ document.getElementById('btn-2').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -57,11 +53,9 @@ document.getElementById('btn-2').addEventListener('click', function(event){
     }
 
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-2");
@@ -69,16 +63,14 @@ document.getElementById('btn-2').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Add Dark Mode at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
-// ----------third button---------------
+// ----------no:3 button---------------
 
 document.getElementById('btn-3').addEventListener('click', function(event){
 
@@ -87,7 +79,7 @@ document.getElementById('btn-3').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -97,11 +89,9 @@ document.getElementById('btn-3').addEventListener('click', function(event){
     }
 
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-3");
@@ -109,16 +99,14 @@ document.getElementById('btn-3').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Optimize  Home page  at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
-// ----------forth button---------------
+// ----------no: 4  button---------------
 
 document.getElementById('btn-4').addEventListener('click', function(event){
 
@@ -127,7 +115,7 @@ document.getElementById('btn-4').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -137,11 +125,9 @@ document.getElementById('btn-4').addEventListener('click', function(event){
     }
 
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-4");
@@ -149,17 +135,15 @@ document.getElementById('btn-4').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Add new emoji 🤲 at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
 
-// ----------fifth button---------------
+// ----------no:5 button---------------
 
 document.getElementById('btn-5').addEventListener('click', function(event){
 
@@ -168,7 +152,7 @@ document.getElementById('btn-5').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -178,11 +162,9 @@ document.getElementById('btn-5').addEventListener('click', function(event){
     }
 
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-5");
@@ -190,16 +172,14 @@ document.getElementById('btn-5').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Integrate OpenAI API  at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
-// ----------sisth button---------------
+// ----------no:6 button---------------
 
 document.getElementById('btn-6').addEventListener('click', function(event){
 
@@ -209,7 +189,7 @@ document.getElementById('btn-6').addEventListener('click', function(event){
     const totalAssigned = document.getElementById('totalAssigned').innerText;
     const convertTotalAssigned = parseInt(totalAssigned);
     if(convertTotalAssigned===1){
-        alert('congrates!!! you complieted add the current task');
+        alert('congrates!!! you complieted all the current task');
         const updatedTotalAssigned = convertTotalAssigned - 1;
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
@@ -218,11 +198,9 @@ document.getElementById('btn-6').addEventListener('click', function(event){
         document.getElementById('totalAssigned').innerText = updatedTotalAssigned;
     }
     const navTotalAssigned = document.getElementById('nav-totalAssigned').innerText;
-    
     const convertnavTotalAssigned = parseInt(navTotalAssigned);
     
     const updatedNavTotalAssigned = convertnavTotalAssigned + 1;
-    console.log(updatedNavTotalAssigned)
     document.getElementById('nav-totalAssigned').innerText = updatedNavTotalAssigned;
 
     const button = document.getElementById("btn-6");
@@ -230,12 +208,10 @@ document.getElementById('btn-6').addEventListener('click', function(event){
     button.style.backgroundColor = '#b0b8e6';
 
     const activityLog = document.createElement('p');
-    
     activityLog.innerText = 'You have Complete The Task Improve Job searching  at 12:48:15 PM';
     activityLog.classList.add('history-style');
     const history = document.getElementById('history');
     history.appendChild(activityLog);
-    console.log(activityLog.classList.value);
 
 })
 
@@ -247,18 +223,23 @@ document.getElementById('clear-btn').addEventListener('click', function(){
     history.innerHTML = '';
 })
 
-
-
 // ------------secend-page-------------
 
 document.getElementById('to-secend-page').addEventListener('click', function(){
     window.location.href = 'scendPage.html';
 })
 
+// ---------------change-color--------------
 
-
-// ------------secend-page  to first-page-------------
-
-document.getElementById('to-first-page').addEventListener('click', function(){
-    console.log('hello ')
+const colors = ["AntiqueWhite", "BurlyWood", "CadetBlue", "Gray ", "DarkKhaki", "Chocolate", "yellow", "Aquamarine", "Sky Blue"];
+let number = 0;
+document.getElementById('btn-color').addEventListener('click', function(){
+    document.body.style.backgroundColor = colors[number];
+    number = (number + 1) % colors.length;
 })
+
+// -------set current date---------------
+
+
+const date = new Date();
+document.getElementById('date-time').innerHTML =date.toDateString();
